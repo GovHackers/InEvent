3f318070eb5357e906bc9517bd437e106b3e31de
Scott Richmond <s.t.richmond@gmail.com>
2014-07-12 22:43:37 +1000

    Remove debugging printline.

--- src/main/java/dataimporter/TagsParser.java
+++ src/main/java/dataimporter/TagsParser.java
@@ -10,7 +10,6 @@ public class TagsParser {
 
     public TagsParser() {
         parseTagsFile();
-        System.out.println("tagsData: " + tagsData.size());
     }
 
     private void parseTagsFile() {
