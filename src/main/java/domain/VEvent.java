6680222200ae69cd7762450a25da706aa29d6254
jrigby <jason.rigby@monash.edu>
2014-07-12 13:09:24 +1000

    VEvent, Venue and GPSCoords added

---
+++ src/main/java/domain/VEvent.java
@@ -0,0 +1,121 @@
+package domain;
+
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by jrigby on 12/07/2014.
+ *
+ * Represents a single event occurrence
+ */
+public class VEvent {
+    private int id;
+    private String title;
+    private URL link;
+    private String description;
+    private String category;
+    private List<String> tag;
+    private Date eventDate;
+    private Venue venue;
+    private List<String> imageUrl;
+    private String contactPhone;
+    private String contactEmail;
+    private boolean isFree;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public URL getLink() {
+        return link;
+    }
+
+    public void setLink(URL link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public List<String> getTag() {
+        return tag;
+    }
+
+    public void setTag(List<String> tag) {
+        this.tag = tag;
+    }
+
+    public Date getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(Date eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public Venue getVenue() {
+        return venue;
+    }
+
+    public void setVenue(Venue venue) {
+        this.venue = venue;
+    }
+
+    public List<String> getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(List<String> imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public boolean isFree() {
+        return isFree;
+    }
+
+    public void setFree(boolean isFree) {
+        this.isFree = isFree;
+    }
+}
