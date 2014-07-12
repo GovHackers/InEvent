d9cdda0b011d250161d3bc0c9661c26e58442117
Ryan Doyle <ryan@doylenet.net>
2014-07-13 04:10:28 +1000

    Fix routing issue

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -11,6 +11,8 @@ public class Application {
     public static final String GET_RELEVANT_EVENTS = "/api/events/get_relevant";
 
     public static void main(String[] args){
+        
+        staticFileLocation("/public");
 
         get(GET_RELEVANT_EVENTS, "application/json", (request, response) -> {
             return new EventsController(request, response).getEvents();
@@ -20,7 +22,6 @@ public class Application {
             response.type("application/json");
         });
 
-        staticFileLocation("/public");
 
     }
 }
