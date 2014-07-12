8ca71123a57b0335405eb2c44ccbf08b23a30a2b
Scott Richmond <s.t.richmond@gmail.com>
2014-07-12 14:43:59 +1000

    Added more stuff.

--- src/main/java/dataimporter/EntryProcessor.java
+++ src/main/java/dataimporter/EntryProcessor.java
@@ -1,16 +1,43 @@
 package dataimporter;
 
 import com.sun.syndication.feed.synd.SyndEntryImpl;
+import domain.VEvent;
+
+import java.util.Date;
+import java.util.List;
 
 public class EntryProcessor {
 
     private SyndEntryImpl entry;
+    private List<VEvent> vEvents;
 
     public EntryProcessor(SyndEntryImpl eventRSSEntry) {
         entry = eventRSSEntry;
+
     }
 
-    public String getTitle() {
+    public List<VEvent> getVEvents() {
+        return vEvents;
+    }
+
+    private void generateEventsFromDates() {
+        for(String date : getDates()) {
+            VEvent vEvent = new VEvent();
+
+            //vEvent.setEventDate(Date(date));
+        }
+    }
+
+    private List<String> getDates() {
+        // TODO
+        return null;
+    }
+
+    private String getTitle() {
         return entry.getTitle();
+    }
+
+    private List<SyndEntryImpl> getInChildren(String elementName) {
+
     }
 }
