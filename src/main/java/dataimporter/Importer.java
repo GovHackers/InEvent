6c39b864b33c7c0dc1c72cbf9bbddafa2523d9a8
jrigby <jason.rigby@monash.edu>
2014-07-12 13:47:22 +1000

    VEvent, Venue and GPSCoords added

--- src/main/java/dataimporter/Importer.java
+++ src/main/java/dataimporter/Importer.java
@@ -1,16 +1,28 @@
 package dataimporter;
+import com.sun.syndication.feed.synd.SyndEntryImpl;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.fetcher.FeedFetcher;
 import com.sun.syndication.fetcher.FetcherException;
 import com.sun.syndication.fetcher.impl.HttpURLFeedFetcher;
 import com.sun.syndication.io.FeedException;
 
+import org.jdom.Element;
+
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Importer {
     public static void main(String[] args) {
-        GetRSSFeed("http://www.eventsvictoria.com/distributionservice/rss.xml");
+
+        SyndFeed f = GetRSSFeed("http://www.eventsvictoria.com/distributionservice/rss.xml");
+        for (SyndEntryImpl entry : (List<SyndEntryImpl>)f.getEntries()) {
+            for (Element element : (ArrayList<Element>)entry.getForeignMarkup()) {
+                System.out.println("NAME: "+element.getName());
+                System.out.println("VALUE: "+element.getValue());
+            }
+        }
     }
 
     void ImportRSSFeed(String feedURL) {
@@ -31,7 +43,6 @@ public class Importer {
         } catch (FetcherException e) {
             e.printStackTrace();
         }
-        System.out.println(feed);
 
         return feed;
     }
