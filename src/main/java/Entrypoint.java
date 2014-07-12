bebfa0f3bcda927d0575bedffbb6149841150b57
jrigby <jason.rigby@monash.edu>
2014-07-12 22:50:30 +1000

    Some messages printed to console.

--- src/main/java/Entrypoint.java
+++ src/main/java/Entrypoint.java
@@ -6,8 +6,11 @@ import dataimporter.Importer;
 public class Entrypoint {
     public static void main(String[] args) {
         if (args.length == 1 && args[0].equals("populate")) {
+            System.out.println("Populating events database...");
             Importer.main(args);
+            System.out.println("Population complete.");
         } else {
+            System.out.println("=== InEvent server ===");
             Application.main(args);
         }
     }
