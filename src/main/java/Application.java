b3ffff3bfc0ff79292c5e80560eb27be4cd15498
Ryan Doyle <ryan@doylenet.net>
2014-09-14 17:42:21 +1000

    Introduce spring DI container

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,7 @@
 import controllers.EventsController;
 import controllers.ManditoryQueryParameterException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import static spark.Spark.after;
 import static spark.Spark.exception;
@@ -9,9 +11,17 @@ import static spark.SparkBase.staticFileLocation;
 public class Application {
 
     public static final String GET_RELEVANT_EVENTS = "/api/events/get_relevant";
+    public static final String INEVENT_HTTP_CONTEXT = "inevent-http.xml";
 
     public static void main(String[] args){
-        
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(INEVENT_HTTP_CONTEXT);
+        Application application = applicationContext.getBean("application", Application.class);
+
+        application.initialiseRoutes();
+    }
+
+    private void initialiseRoutes() {
         staticFileLocation("/public");
 
         get(GET_RELEVANT_EVENTS, "application/json", (request, response) -> {
@@ -21,7 +31,5 @@ public class Application {
         after(GET_RELEVANT_EVENTS, (request, response) -> {
             response.type("application/json");
         });
-
-
     }
 }
